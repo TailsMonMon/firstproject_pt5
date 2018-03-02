3ce084a03a56a24dd4470a650840195ce0749c2b
Monica Björk <m.bjork95@gmail.com>
2018-03-02 20:17:42 +0100

    Just started

--- firstproject_pt5/Program.cs
+++ firstproject_pt5/Program.cs
@@ -7,7 +7,34 @@ using System.Threading.Tasks;
 namespace firstproject_pt5 {
     class Program {
         static void Main(string[] args) {
+            Rectangle rect1;
+            rect1.length=10;
+            rect1.width=5;
+            Console.WriteLine("Area of rect1 = {0}", rect1.Area());
 
+            Rectangle rect2 = new Rectangle(3, 3);
+
+            rect2=rect1;
+            rect1.length=63;    // To prove it's a reference, not a value.
+
+            Console.WriteLine("rect2.length = {0}", rect2.length);
+
+            Console.ReadLine();
         }
+
+        struct Rectangle {
+            public double length;
+            public double width; 
+
+            public Rectangle(double l = 1, double w = 1) {
+                length=l;
+                width=w;
+            }
+
+            public double Area() {
+                return length*width;
+            }
+        }
+
     }
 }
