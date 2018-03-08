faba32c100311d31993e116d56bf72b1340f7fd5
Monica Björk <m.bjork95@gmail.com>
2018-03-08 17:23:17 +0100

    Added some more comments.

--- firstproject_pt5/ShapeMath.cs
+++ firstproject_pt5/ShapeMath.cs
@@ -3,17 +3,17 @@
 namespace firstproject_pt5 {
     public static class ShapeMath {
         public static double GetArea(string shape="", 
-            double length1=0, 
-            double length2=0 ) {
+            double length1=0, // x
+            double length2=0 ) { // y
 
             if (String.Equals("Rectangle", shape, StringComparison.OrdinalIgnoreCase)) {
-                return length1*length2;
+                return length1*length2; // x*y
             }
             else if (String.Equals("Triangle", shape, StringComparison.OrdinalIgnoreCase)) {
-                return length1*(length2/2);
+                return length1*(length2/2); // x/y
             }
             else if(String.Equals("Circle", shape, StringComparison.OrdinalIgnoreCase)) {
-                return 3.14159*Math.Pow(length1, 2);
+                return 3.14159*Math.Pow(length1, 2); // pi*x
             }
             else {
                 return -1;
